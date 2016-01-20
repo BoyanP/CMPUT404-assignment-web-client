2999401a58e19f1ee093389b5bb523c8df3aa90c
BoyanP <bgpeychoff@gmail.com>
2016-01-20 14:41:36 -0700

    commented in where I think things should go

--- httpclient.py
+++ httpclient.py
@@ -36,16 +36,26 @@ class HTTPClient(object):
     #def get_host_port(self,url):
 
     def connect(self, host, port):
+        clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        clientSocket.connect(host,port)
         # use sockets!
         return None
 
     def get_code(self, data):
+        #get status code
+        #parse it from the first line
         return None
 
     def get_headers(self,data):
+        #get response headers
+        #probably used most for post
         return None
 
     def get_body(self, data):
+        #get body of the httprequest
+        #so add stuff like accept
+        #and where you are going
+        #or what variables you are posting
         return None
 
     # read everything from the socket
@@ -61,16 +71,35 @@ class HTTPClient(object):
         return str(buffer)
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        #code = 500
+        #body = ""
+        #--- initial code is above----
+        
+        #code = args[0]
+        #headers = args[1]
+        #body = args[2]
+
         return HTTPRequest(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        #code = 500
+        #body = ""
+        #--- initial code is above----
+        
+        #code = args[0]
+        #headers = args[1]
+        #body = args[2]
+        
         return HTTPRequest(code, body)
 
-    def command(self, url, command="GET", args=None):
+    def command(self,url ,command="GET" ,args=None):
+        #sock = 80
+        #self.connect(url,sock)
+        #data = self.recvall(sock)
+        #code = self.get_code(data)
+        #headers=get_headers(data)
+        #body = get_body(data)
+        #args = [code,headers,body]
         if (command == "POST"):
             return self.POST( url, args )
         else:
@@ -85,4 +114,4 @@ if __name__ == "__main__":
     elif (len(sys.argv) == 3):
         print client.command( sys.argv[1], sys.argv[2] )
     else:
-        print client.command( command, sys.argv[1] )    
+        print client.command( command, sys.argv[1] )
